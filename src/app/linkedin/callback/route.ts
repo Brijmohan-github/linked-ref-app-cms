14601515f57f12ff3b4bd2521bfce02f6f049d76
Brijmohan karadia <bmk302006@gmail.com>
2026-09-04 13:14:01 +0530

    dynamic redirection

--- src/app/linkedin/callback/route.ts
+++ src/app/linkedin/callback/route.ts
@@ -1,131 +1,123 @@
-    import axios from "axios";
-    import { NextResponse } from "next/server";
-    import jwt from "jsonwebtoken";
-    import UserService from "@/lib/UserService";
-    // var lsinkedinScraper = require("linkedin-scraper"); 
+import axios from "axios";
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import UserService from "@/lib/UserService";
+// var lsinkedinScraper = require("linkedin-scraper");
 
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
 
-    export async function GET(req: Request) {
+  console.log("🚀 Brij  ~  GET ~  code:", code);
+
+  if (!code) {
+    return NextResponse.json({ error: "Missing code" }, { status: 400 });
+  }
+
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("client_id", process.env.LINKEDIN_CLIENT_ID!);
+    params.append("client_secret", process.env.LINKEDIN_CLIENT_SECRET!);
+    params.append("redirect_uri", process.env.LINKEDIN_REDIRECT_URI!);
+    console.log("🚀 Brij  ~  GET ~  params:", params.toString());
+
+    const tokenResponse = await axios.post(
+      "https://www.linkedin.com/oauth/v2/accessToken",
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+    const accessToken = tokenResponse.data.access_token;
+    console.log("at line no 58 ", accessToken);
+
+    // linkedinScraper("https://www.linkedin.com/in/brijmohan-k-10304b418/",
+    //     function (linkedinObject: any) {
+    //     console.log(linkedinObject);
+    //     }
+    // );
+
+    const userResponse = await axios.get(
+      "https://api.linkedin.com/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    //  const userResponseMyInfo = await axios.get(
+    //     "https://api.linkedin.com/v2/people/me",
+    //     {
+    //     headers: {
+    //         Authorization: `Bearer ${accessToken}`,
+    //     },
+    //     }
+    // );
+
+    // console.log("at line no 60", userResponseMyInfo.data);
+    const getresponse = await UserService.createOrUpdateLinkedinUser(
+      userResponse.data,
+      accessToken,
+    );
+
+    const user = userResponse.data;
+    console.log("LinkedIn user payload:", user);
+    console.log("Saved user:", getresponse);
+
+    // const jwt =  process.env.JWT_SECRET! + '@@' + user.name + '@@' + user.sub;
+
+    const payload = {
+      sub: user.sub, // LinkedIn user id
+      name: user.name,
+      email: user.email,
+    };
+
+    // Create JWT valid for 7 days
+    const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    const userAgent = req.headers["user-agent"] || "";
+
+    let device = "unknown";
+
+    if (/android/i.test(userAgent)) {
+      device = "android";
+    } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+      device = "ios";
+    } else if (/windows/i.test(userAgent)) {
+      device = "windows";
+    } else if (/macintosh/i.test(userAgent)) {
+      device = "macos";
+    }
+
+    if (device === "android" || device === "ios") {
+      return NextResponse.redirect(
+        `refhubsapp://linkedin?token=${encodeURIComponent(token)}&userdata=${encodeURIComponent(JSON.stringify(user))}`,
+      );
+    } else {
+     return NextResponse.redirect(
+      process.env.APP_URL +
+        "/auth?token=" +
+        encodeURIComponent(token) +
+        "&userdata=" +
+        encodeURIComponent(JSON.stringify(user)),
+    );
+    }
     
-        const { searchParams } = new URL(req.url);
-        const code = searchParams.get("code");
+  } catch (error: any) {
+    console.error("At line no 135 ", error.response?.data);
 
-        console.log("🚀 Brij  ~  GET ~  code:", code);
-
-
-        if (!code) {
-            return NextResponse.json(
-            { error: "Missing code" },
-            { status: 400 }
-            );
-        }
-
-        try {
-            const params = new URLSearchParams();
-            params.append(
-            "grant_type",
-            "authorization_code"
-            );
-            params.append("code", code);
-            params.append(
-            "client_id",
-            process.env.LINKEDIN_CLIENT_ID!
-            );
-            params.append(
-            "client_secret",
-            process.env.LINKEDIN_CLIENT_SECRET!
-            );
-            params.append(
-            "redirect_uri",
-            process.env.LINKEDIN_REDIRECT_URI!
-            );
-            console.log("🚀 Brij  ~  GET ~  params:", params.toString());
-
-            const tokenResponse =
-            await axios.post(
-                "https://www.linkedin.com/oauth/v2/accessToken",
-                params,
-                {
-                headers: {
-                    "Content-Type":
-                    "application/x-www-form-urlencoded",
-                },
-                }
-            );
-            const accessToken = tokenResponse.data.access_token;
-            console.log("at line no 58 ",accessToken);
-
-            // linkedinScraper("https://www.linkedin.com/in/brijmohan-k-10304b418/",
-            //     function (linkedinObject: any) {
-            //     console.log(linkedinObject);
-            //     }
-            // );
-
-
-            const userResponse = await axios.get(
-                "https://api.linkedin.com/v2/userinfo",
-                {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-                }
-            );
-
-            //  const userResponseMyInfo = await axios.get(
-            //     "https://api.linkedin.com/v2/people/me",
-            //     {
-            //     headers: {
-            //         Authorization: `Bearer ${accessToken}`,
-            //     },
-            //     }
-            // );
-
-            // console.log("at line no 60", userResponseMyInfo.data);
-            const getresponse = await UserService.createOrUpdateLinkedinUser(
-              userResponse.data, accessToken
-            );
-
-            const user = userResponse.data;
-            console.log("LinkedIn user payload:", user);
-            console.log("Saved user:", getresponse);
-
-           
-            // const jwt =  process.env.JWT_SECRET! + '@@' + user.name + '@@' + user.sub;
-
-       
-            const payload = {
-                sub: user.sub,          // LinkedIn user id
-                name: user.name,
-                email: user.email,
-            };
-
-            // Create JWT valid for 7 days
-            const token = jwt.sign(
-                payload,
-                process.env.JWT_SECRET!,
-                {
-                    expiresIn: "7d",
-                }
-            );
-
- 
-             return NextResponse.redirect(
-                 `refhubsapp://linkedin?token=${encodeURIComponent(token)}&userdata=${encodeURIComponent(JSON.stringify(user))}`
-            );
-
-            ////////web redirection ////////
-                // return NextResponse.redirect(process.env.APP_URL + "/auth?token=" + encodeURIComponent(token) + "&userdata=" + encodeURIComponent(JSON.stringify(user)));    
-             ////////web redirection ////////
-
-        } catch (error: any) {
-            console.error("At line no 135 ",error.response?.data);
-
-            return NextResponse.json(
-            {
-                error:
-                error.response?.data || error.message,
-            },
-            { status: 500 }
-            );
-        }
-    } 
+    return NextResponse.json(
+      {
+        error: error.response?.data || error.message,
+      },
+      { status: 500 },
+    );
+  }
+}
